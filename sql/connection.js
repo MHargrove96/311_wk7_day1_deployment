a91e00c9cb9eab6e7b8784cc5387c747553256ac
MHargrove96 <86787734+MHargrove96@users.noreply.github.com>
2022-04-01 19:11:20 -0500

    first commit

--- sql/connection.js
+++ sql/connection.js
@@ -1,32 +1,34 @@
-const mysql = require('mysql')
+require("dotenv").config();
+const mysql = require("mysql");
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DEFAULT_SCHEMA } = process.env;
 
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating mysql connection...')
+      console.log("creating mysql connection...");
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DEFAULT_SCHEMA,
+      };
+
+      if (process.env.NODE_ENV === "production" && process.env.CLOUD_INSTANCE) {
+        console.log(`connect socket: ${process.env.CLOUD_INSTANCE}`);
+        config.socketPath = `/cloudsql/${process.env.CLOUD_INSTANCE}`;
       }
 
-      if (process.env.NODE_ENV === 'production' && process.env.CLOUD_INSTANCE) {
-        console.log(`connect socket: ${process.env.CLOUD_INSTANCE}`)
-        config.socketPath = `/cloudsql/${process.env.CLOUD_INSTANCE}`
-      }
+      this.pool = mysql.createPool(config);
 
-      this.pool = mysql.createPool(config)
-
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
